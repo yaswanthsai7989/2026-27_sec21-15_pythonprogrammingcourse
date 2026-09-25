c80ee5a227069f0dffed2443b1aef4be0e459960
yaswanth sai <saioffical7989@gmail.com>
2026-09-25 16:17:03 +0530

    Add files via upload

---
+++ unit2task7.py
@@ -0,0 +1,12 @@
+# Unit-2A_Task-7_Student_Grade_Elif_Ladder.py
+
+marks = int(input("Enter marks: "))
+
+if marks >= 90:
+    print("Grade A")
+elif marks >= 80:
+    print("Grade B")
+elif marks >= 70:
+    print("Grade C")
+else:
+    print("Grade D")
