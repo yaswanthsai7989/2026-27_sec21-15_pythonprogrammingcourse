bb099187d83499522457d3319ac80753ed7327de
yaswanth sai <saioffical7989@gmail.com>
2026-09-25 15:33:07 +0530

    Add files via upload

---
+++ task13.py
@@ -0,0 +1,6 @@
+numbers = [10, 20, 30, 40, 50]
+
+print("20 in numbers:", 20 in numbers)
+print("60 in numbers:", 60 in numbers)
+print("20 not in numbers:", 20 not in numbers)
+print("60 not in numbers:", 60 not in numbers)
