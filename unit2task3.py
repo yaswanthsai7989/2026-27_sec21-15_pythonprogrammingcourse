cb4e2275ff2cafbd73207a68d994624540f8be7e
yaswanth sai <saioffical7989@gmail.com>
2026-09-25 16:11:00 +0530

    Add files via upload

---
+++ unit2task3.py
@@ -0,0 +1,18 @@
+# Unit-2A_Task-3_Largest_of_Three_Nested_If.py
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+c = int(input("Enter third number: "))
+
+if a > b:
+    if a > c:
+        largest = a
+    else:
+        largest = c
+else:
+    if b > c:
+        largest = b
+    else:
+        largest = c
+
+print("Largest number is:", largest)
