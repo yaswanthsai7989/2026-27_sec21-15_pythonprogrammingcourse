3180c3ad15fb8c7b1bbc3e78c93e45d03219e90b
yaswanth sai <saioffical7989@gmail.com>
2026-09-25 15:57:16 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,15 @@
+age  = int(input("Enter age in years: "))
+
+
+
+
+
+if age >= 18:
+    print(f"Age {age} years: Eligible to vote")
+
+
+
+
+
+else:
+    print(f"Age {age} years: Not eligible to vote")
