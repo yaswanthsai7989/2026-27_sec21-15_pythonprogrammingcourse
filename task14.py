322202002b6ec0f8902f9b018338f804e6a10078
yaswanth sai <saioffical7989@gmail.com>
2026-09-25 15:36:03 +0530

    Add files via upload

---
+++ task14.py
@@ -0,0 +1,13 @@
+# Operator Precedence
+result1 = 10 + 5 * 2
+print("10 + 5 * 2 =", result1)
+
+result2 = (10 + 5) * 2
+print("(10 + 5) * 2 =", result2)
+
+# Operator Associativity
+result3 = 10 - 5 - 2
+print("10 - 5 - 2 =", result3)
+
+result4 = 2 ** 3 ** 2
+print("2 ** 3 ** 2 =", result4)
