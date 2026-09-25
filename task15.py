0f62491ff61c1085888dc10e5ffb08829e5f9e78
yaswanth sai <saioffical7989@gmail.com>
2026-09-25 15:37:17 +0530

    Add files via upload

---
+++ task15.py
@@ -0,0 +1,19 @@
+# String to Integer
+a = "10"
+b = int(a)
+print("String to Integer:", b)
+
+# Integer to Float
+c = 20
+d = float(c)
+print("Integer to Float:", d)
+
+# Float to Integer
+e = 15.5
+f = int(e)
+print("Float to Integer:", f)
+
+# Integer to String
+g = 25
+h = str(g)
+print("Integer to String:", h)
