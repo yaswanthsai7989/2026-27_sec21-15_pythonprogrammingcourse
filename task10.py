8d24957b28b9e736c40b23310902ca9ec5081522
yaswanth sai <saioffical7989@gmail.com>
2026-09-25 15:27:48 +0530

    Add files via upload

---
+++ task10.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("a == b:", a == b)
+print("a != b:", a != b)
+print("a > b:", a > b)
+print("a < b:", a < b)
+print("a >= b:", a >= b)
+print("a <= b:", a <= b)
